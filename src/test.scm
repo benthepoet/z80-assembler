5ca7c737c131805e90493dcd1be6a946ab09fb3c
Ben Hanna <benpaulhanna@gmail.com>
2022-09-25 12:22:42 -0400

    Test function for counting bytes.

--- src/test.scm
+++ src/test.scm
@@ -1,6 +1,14 @@
 (define-macro (inc! var)
   `(set! ,var (+ ,var 1)))
 
-(define a 2)
-(inc! a)
-(pp a)
+(define count-bytes
+  (lambda (n)
+    (let loop ((i 0)
+               (x n))
+      (set! x (arithmetic-shift x -8))
+      (inc! i)
+      (if (= x 0)
+          i
+          (loop i x)))))
+            
+          
