6f7a2ff02295b9d7c6368b25b4cde1ed3b27b0c7
Ben Hanna <benpaulhanna@gmail.com>
2022-12-18 13:05:54 -0500

    Started new implementation in JavaScript.

---
+++ src/main.js
@@ -0,0 +1,98 @@
+const MNEMONICS = {
+	dec: inc_dec,
+	inc: inc_dec
+};
+
+const PATTERNS = {
+	inc_dec_group_1: [
+		{
+			pattern: ['r1'],
+			prefix: 00,
+			base: 04
+		}
+	],
+	inc_dec_group_2: [
+		{
+			pattern: ['ss'],
+			prefix: 00,
+			base: 03
+		}
+	]
+};
+
+const MATCH_TABLES = {
+	r1: ['b','c','d','e','h','l','a'],
+	ss: ['bc','de','hl','sp']
+};
+
+let opcode_shifts = [];
+
+function match_group_1(token, sym) {
+	if (sym === 'r1') {
+		let i = MATCH_TABLES.r1.indexOf(token);
+		if (i === -1) return false;
+		else if (i === 6) i++;
+		opcode_shifts.push(i << 3);
+		return true;
+	}
+
+	return token === sym;
+}
+
+function match_group_2(token, sym) {
+	if (sym === 'ss') {
+		let i = MATCH_TABLES.ss.indexOf(token);
+		if (i === -1) return false;
+		opcode_shifts.push(i << 4);
+		return true;
+	}
+
+	return token === sym;
+}
+
+function find_pattern(tokens, patterns, match_fn) {
+	for (const p of patterns)
+	{
+		if (tokens.length !== p.pattern.length) continue;
+		opcode_shifts = [];
+
+		let matched = false;
+		for (let i = 0; i < tokens.length; i++) {
+			matched = match_fn(tokens[i], p.pattern[i]);
+			if (!matched) break;
+		}
+
+		if (matched) return p;
+	}
+
+	return null;
+}
+
+function inc_dec(mnemonic, tokens) {
+	let opcode = null;
+	let pattern = null;
+	if ((pattern = find_pattern(tokens, PATTERNS.inc_dec_group_1, match_group_1)) !== null) {
+		opcode = pattern.base;
+		if (mnemonic === 'dec') opcode |= 1;
+	}
+	else if ((pattern = find_pattern(tokens, PATTERNS.inc_dec_group_2, match_group_2)) !== null) {
+		opcode = pattern.base;
+		if (mnemonic === 'dec') opcode |= 8;
+	}
+
+	if (opcode !== null) {
+		for (const s of opcode_shifts) {
+			opcode |= s;
+		}
+
+		return [toHex(pattern.prefix), toHex(opcode)];
+	}
+
+	throw Error("Failed to match any pattern.");
+}
+
+function toHex(value) {
+	return value.toString(16);
+}
+
+console.log(inc_dec('dec', ['sp']));
