a17505f0c2e2885e55a5311f565ac2c58c1b2060
Ben Hanna <benpaulhanna@gmail.com>
2022-12-29 12:25:28 -0500

    Added address to output.

--- src/cli.js
+++ src/cli.js
@@ -8,6 +8,7 @@ if (process.argv.length < 3) {
 	throw Error("Filename not supplied.");
 }
 var filename = process.argv[2];
+var location_counter = 0;
 
 async function run() {
 	try {
@@ -20,7 +21,8 @@ async function run() {
 			var bytes = assembler.assemble(line);
 			if (bytes.length > 0) {
 				var s = bytes.map(n => n.toString(16).padStart(2, '0')).join(' ');
-				console.log(s.padEnd(12, ' '), line);
+				console.log(location_counter.toString(16).padStart(4, '0'), s.padEnd(12, ' '), line);
+				location_counter += bytes.length;
 			}
 		});
 
