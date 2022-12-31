16163f3585f576334134e00dce40411ea257c58d
Ben Hanna <benpaulhanna@gmail.com>
2022-12-31 12:33:19 -0500

    Closed file handles.

--- src/cli.mjs
+++ src/cli.mjs
@@ -43,31 +43,37 @@ function print_brk() {
 }
 
 async function process(output) {
-	var src_file = await open(values.src_filename, 'r');
-		
-	var bin_file = null;
-	if (values.bin_filename != null) {
-		bin_file = await open(values.bin_filename, 'w' );
-	}
+	try {
+		var src_file = await open(values.src_filename, 'r');
+			
+		var bin_file = null;
+		if (values.bin_filename != null) {
+			bin_file = await open(values.bin_filename, 'w' );
+		}
 
-	var location_counter = init_location();
-	for await (var line of src_file.readLines()) {
-		var bytes = assemble(line);
-		if (bytes.length > 0) {
-			if (output && bin_file !== null) {
-				await bin_file.write(Buffer.from(bytes));
+		var location_counter = init_location();
+		for await (var line of src_file.readLines()) {
+			var bytes = assemble(line);
+			if (bytes.length > 0) {
+				if (output && bin_file !== null) {
+					await bin_file.write(Buffer.from(bytes));
+				}
+
+				location_counter = inc_location(bytes.length);
 			}
 
-			location_counter = inc_location(bytes.length);
+			if (output && values.verbose) {
+				var byteString = bytes.map(n => n.toString(16).padStart(2, '0')).join(' ');
+				console.log(location_counter.toString(16).padStart(4, '0'), byteString.padEnd(12, ' '), line);
+			}
 		}
 
-		if (output && values.verbose) {
-			var byteString = bytes.map(n => n.toString(16).padStart(2, '0')).join(' ');
-			console.log(location_counter.toString(16).padStart(4, '0'), byteString.padEnd(12, ' '), line);
+		if (values.verbose) {
+			console.log(get_state(), '\n');
 		}
 	}
-
-	if (values.verbose) {
-		console.log(get_state(), '\n');
+	finally {
+		await src_file?.close();
+		await bin_file?.close();
 	}
 }
