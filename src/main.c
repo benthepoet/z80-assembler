40bdba07ed87d88c4fdbfde0fcd6bbf0a52b9510
Ben Hanna <benpaulhanna@gmail.com>
2023-09-17 19:09:45 -0400

    Cleaned up organization.

--- src/main.c
+++ src/main.c
@@ -4,25 +4,28 @@
 
 typedef unsigned char byte;
 
-bool need_padding(byte a, byte b) {
+bool is_unpadded(byte a, byte b) {
     return a != ' ' && b >= '(' && b <= '+';
+}
+
+byte normalize_whitespace(byte b) {
+    return b == ',' || b == '\t' ? ' ' : b;
 }
 
 char *format_line(char const *line, char *buf) {
     byte i, n = 0;
     int k = strlen(line) - 1;
     for (; i < k; i++) {
-        byte a = line[i];
-        byte b = line[i + 1];
-        a = a == ',' || a == '\t' ? ' ' : a;
-
+        byte a = normalize_whitespace(line[i]);
+        byte b = normalize_whitespace(line[i + 1]);
+        
         if (a == b && a == ' ') {
             continue;
         }
         
         buf[n++] = a;
         
-        if (need_padding(a, b) || need_padding(b, a)) {
+        if (is_unpadded(a, b) || is_unpadded(b, a)) {
             buf[n++] = ' ';
         }
     }
@@ -33,7 +36,7 @@ char *format_line(char const *line, char *buf) {
 
 
 int main() {
-    char s[] = "ld     (ix+6)   ,h";
+    char s[] = "ld     (ix+6)   ,,,,h";
     char o[32];
     format_line(s, o);
 
