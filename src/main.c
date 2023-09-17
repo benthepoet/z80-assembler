5a38a88ab333af9e6ac8c0c7a9226d2a9b4c4c6a
Ben Hanna <benpaulhanna@gmail.com>
2023-09-17 16:57:16 -0400

    Updated C implementation.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+bool should_pad(unsigned char c) {
+    return c >= '(' && c <= '+';
+}
 
 char *format_line(char const *line, char *buf) {
     int n = 0;
     int k = strlen(line);
     for (int i = 0; i < k; i++) {
         unsigned char a = line[i];
+        a = a == ',' || a == '\t' ? ' ' : a;
         buf[n++] = a;
 
         int j = i + 1;
         if (j < k) {
             unsigned char b = line[j];
-            if ((a != ' ' && (b >= '(' && b <= '+'))
-                || (b != ' ' && (a >= '(' && a <= '+'))) {
+            if ((a != ' ' && should_pad(b))
+                || (b != ' ' && should_pad(a))) {
                 buf[n++] = ' ';
             }
         }
     }
 }
+
+
 
 int main() {
     char s[] = "ld (ix+6),h";
