a131f77fdd78eebd4a256a05a50e5cbb9ef7b913
Ben Hanna <benpaulhanna@gmail.com>
2023-09-17 19:05:46 -0400

    Added space compaction.

--- src/main.c
+++ src/main.c
@@ -2,32 +2,39 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool should_pad(unsigned char c) {
-    return c >= '(' && c <= '+';
+typedef unsigned char byte;
+
+bool need_padding(byte a, byte b) {
+    return a != ' ' && b >= '(' && b <= '+';
 }
 
 char *format_line(char const *line, char *buf) {
-    int i, n = 0;
+    byte i, n = 0;
     int k = strlen(line) - 1;
     for (; i < k; i++) {
-        unsigned char a = line[i];
+        byte a = line[i];
+        byte b = line[i + 1];
         a = a == ',' || a == '\t' ? ' ' : a;
-        buf[n++] = a;
 
-        unsigned char b = line[i + 1];
-        if ((a != ' ' && should_pad(b))
-            || (b != ' ' && should_pad(a))) {
+        if (a == b && a == ' ') {
+            continue;
+        }
+        
+        buf[n++] = a;
+        
+        if (need_padding(a, b) || need_padding(b, a)) {
             buf[n++] = ' ';
         }
     }
-    buf[n] = line[i];
+    buf[n++] = line[i];
+    buf[n] = '\0';
 }
 
 
 
 int main() {
-    char s[] = "ld (ix+6),h";
-    char o[80];
+    char s[] = "ld     (ix+6)   ,h";
+    char o[32];
     format_line(s, o);
 
     printf("%s\n", o);
